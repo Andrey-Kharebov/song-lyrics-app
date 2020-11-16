c2f150e0f7d74a6474ee5774b592abc2955588ac
Andrey Kharebov <andreykharebov@gmail.com>
2020-11-16 19:34:48 +0300

    Added if (process.env.NODE_ENV === 'production') options

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const keys = require('./keys/index');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const app = express();
 
 app.use(express.json({ extended: true }));
 
 app.use('/', require('./routes/app.routes'));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use('/', express.static(path.join(__dirname, 'client', 'build')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
 
 const PORT = process.env.PORT || 5000;
 
